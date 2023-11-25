8353179f1f07b079afabe887ac3bee6804dd2891
Claire Sabin <claires@outlook.fr>
2023-11-25 12:41:28 -0500

    file update

--- ml_logic/preprocessor.py
+++ ml_logic/preprocessor.py
@@ -3,6 +3,7 @@ import numpy as np
 from tensorflow import keras
 from keras.preprocessing.image import img_to_array
 from PIL import Image
+from keras import Model, Sequential, layers
 
 def load_and_preprocess_image(uploaded_image):
     """
@@ -12,3 +13,21 @@ def load_and_preprocess_image(uploaded_image):
     image = img_to_array(image)
     image = image / 255.0  # Normalize to [0, 1]
     return image
+<<<<<<< Updated upstream
+=======
+
+
+def create_augmented_model(input_shape=(150, 150, 3)):
+
+    """
+    Image augmnetation function Layers
+    """
+    model = Sequential()
+    model.add(layers.Rescaling(1./255, input_shape=input_shape))
+    model.add(layers.RandomFlip("horizontal"))
+    model.add(layers.RandomZoom(0.1))
+    model.add(layers.RandomTranslation(0.2, 0.2))
+    model.add(layers.RandomRotation(0.1))
+
+    return model
+>>>>>>> Stashed changes
