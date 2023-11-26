6e35bf666dc46aaa714bb132b5d87a7f0ec1c63b
saeed afghah <saeedafghah@gmail.com>
2023-11-25 19:20:58 -0500

    adding files to merge

---
+++ ml_logic/preprocessor.py
@@ -0,0 +1,59 @@
+import os
+import numpy as np
+from tensorflow import keras
+# from keras.preprocessing.image import img_to_array
+from tensorflow.keras.utils import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+from PIL import Image
+from keras import Model, Sequential, layers
+import pandas as pd
+from keras.preprocessing import image
+
+def load_and_preprocess_image(uploaded_image):
+    """
+    Load and preprocess an image given its ID.
+    """
+    image = Image.open(uploaded_image)
+    image = img_to_array(image)
+    image = image / 255.0  # Normalize to [0, 1]
+    return image
+
+# Create a function to balance the data.
+def data_balancing(table_link='../data/'):
+    '''
+    This function loads the raw data and returns a balanced dataset as two
+    dictionaries.
+    '''
+    df = pd.read_csv(f'{table_link}RFMiD_Training_Labels.csv')
+    df_binary = df.loc[:, df.columns.intersection(['ID','Disease_Risk'])]
+    df_healthy = df_binary[df.Disease_Risk == 0]
+    df_unhealthy = df_binary[df.Disease_Risk == 1]
+    df_unhealthy_rndselection = df_unhealthy.sample(n = 401)
+    frames = [df_healthy, df_unhealthy_rndselection]
+    result = pd.concat(frames)
+    return result
+
+def load_and_preprocess_image(image_id, image_folder, target_size=(224, 224)):
+    """
+    Load and preprocess an image given its ID.
+    """
+    image_path = os.path.join(image_folder, f'{image_id}.png')
+    image = load_img(image_path, target_size=target_size)
+    image = img_to_array(image)
+    image = image / 255.0
+    return image
+
+def create_augmented_model(input_shape=(150, 150, 3)):
+
+    """
+    Image augmnetation function Layers
+    """
+
+    data_augmentation = Sequential()
+    data_augmentation.add(layers.Rescaling(1./255, input_shape=input_shape))
+    data_augmentation.add(layers.RandomFlip("horizontal"))
+    data_augmentation.add(layers.RandomZoom(0.1))
+    data_augmentation.add(layers.RandomTranslation(0.2, 0.2))
+    data_augmentation.add(layers.RandomRotation(0.1))
+
+    return data_augmentation
---
+++ ml_logic/registry.py
@@ -0,0 +1,38 @@
+import os
+import time
+import pickle
+
+#from colorama import Fore, Style
+from tensorflow import keras
+from params import *
+
+def save_model(model: keras.Model = None) -> None:
+    """
+    Persist trained model locally on the hard drive at f"{LOCAL_REGISTRY_PATH}/models/{timestamp}.h5"
+    - if MODEL_TARGET='gcs', also persist it in your bucket on GCS at "models/{timestamp}.h5" --> unit 02 only
+    - if MODEL_TARGET='mlflow', also persist it on MLflow instead of GCS (for unit 0703 only) --> unit 03 only
+    """
+
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+
+    # Save model locally
+    model_path = os.path.join(LOCAL_REGISTRY_PATH, "models", f"{timestamp}.h5")
+    model.save(model_path)
+
+    print("✅ Model saved locally")
+
+    return None
+
+
+def load_model(stage="Production") -> keras.Model:
+    """
+    Return a saved model:
+    - locally (latest one in alphabetical order)
+    - or from GCS (most recent one) if MODEL_TARGET=='gcs'  --> for unit 02 only
+    - or from MLFLOW (by "stage") if MODEL_TARGET=='mlflow' --> for unit 03 only
+
+    Return None (but do not Raise) if no model is found
+
+    """
+
+    #return latest_model
