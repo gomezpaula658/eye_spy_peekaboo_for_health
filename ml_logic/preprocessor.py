3ebb9ae0c876b637b2c67c35f73bb4b3141e546f
saeed afghah <saeedafghah@gmail.com>
2023-12-03 15:01:46 -0500

    updated preprocessor file with stage 2 preprocessing function.

--- ml_logic/preprocessor.py
+++ ml_logic/preprocessor.py
@@ -13,7 +13,6 @@ def load_and_preprocess_image_uploaded(uploaded_image):
     """
     image = Image.open(uploaded_image)
     image = img_to_array(image)
-    image = image.reshape(224, 224, 3)
     image = image / 255.0  # Normalize to [0, 1]
     return image
 
@@ -29,11 +28,10 @@ def load_and_preprocess_images(image_id, image_folder, target_size=(224, 224)):
 
 # Create a function to balance the data.
 def data_balancing(table_link='../data/'):
-    """
+    '''
     This function loads the raw data and returns a balanced dataset as two
     dictionaries.
     '''
-    """
     df = pd.read_csv(f'{table_link}RFMiD_Training_Labels.csv').set_index('ID')
     df_binary = df.loc[:, df.columns.intersection(['Disease_Risk'])]
     df_healthy = df_binary[df.Disease_Risk == 0]
@@ -60,8 +58,8 @@ def image_augmentation(images):
     data_augmentation = models.Sequential([
     # layers.Rescaling(1./255),
     layers.RandomFlip("horizontal"),
-    # layers.RandomZoom(0.1),
-    layers.RandomTranslation(0.2, 0.2),
+    layers.RandomZoom(0.001),
+    layers.RandomTranslation(0.02, 0.02),
     layers.RandomRotation(0.1)
     ])
 
@@ -82,6 +80,7 @@ def create_model(shape=tuple):
     image_input = layers.Input(shape=shape)
 
     x = layers.Conv2D(32, (3, 3), activation='relu')(image_input)
+    x = layers.Conv2D(64, (3, 3), activation='relu')(x)
     x = layers.MaxPooling2D(2, 2)(x)
     x = layers.Flatten()(x)
 
@@ -92,6 +91,7 @@ def create_model(shape=tuple):
     model = Model(inputs=image_input, outputs=z)
 
     return model
+
 
 # Stage 2: Create a function to categorize the diseases.
 def disease_categorization(table_link='../data/', cut_off=80):
