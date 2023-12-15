5a6a459a6c04d63b16caad0ad813c8472c5bf49d
Annand Virk <annandvirk@gmail.com>
2023-12-14 21:45:47 -0500

    Made changes to structure of Streamlit app.

--- streamit_eye_app.py
+++ streamit_eye_app.py
@@ -3,15 +3,16 @@ import os
 import glob
 import pandas as pd
 import numpy as np
-from io import StringIO
-from PIL import Image
+import tensorflow as tf
+from io import StringIO, BytesIO
+from PIL import Image, ImageDraw, ImageOps
 from ml_logic.preprocessor import load_and_preprocess_image_uploaded, load_and_preprocess_images
 from ml_logic.registry import load_model
 from params import *
 from ml_logic.model import *
 from ml_logic.registry import *
+from keras import models
 from keras.utils import load_img, img_to_array
-from tensorflow import keras
 from tempfile import NamedTemporaryFile
 
 
@@ -19,34 +20,70 @@ from tempfile import NamedTemporaryFile
 ## and upload their eye scan image. The interface then return the result i.e healthy/not Healthy.
 ## This function will return the downloaded image.
 
-
 def pred(image_processed):
     """Display the result"""
-    model = keras.models.load_model("models/model_1.h5")
+    model = models.load_model("models/model_1.h5")
     image_processed = image_processed.reshape((1,224,224,3))
     prediction_result = model.predict(image_processed)
     #getting class index with highest probability
-    st.write(prediction_result)
+    # st.write(prediction_result)
     print(prediction_result)
     if prediction_result > 0.50:
-        return "Unhealthy"
+        return "**Unhealthy**"
     else:
-        return "Healthy"
+        return "**Healthy**"
 
 #stage 2 - predict disease if eye is unhealthy
 def pred2(image_processed):
-    model1 = keras.models.load_model("models/model_2.h5")
+    model1 = models.load_model("models/model_2.h5")
     image_processed = image_processed.reshape((1,224,224,3))
     prediction_result1 = model1.predict(image_processed)
     #getting class index with highest probability
-    st.write(prediction_result1)
+    # st.write(prediction_result1)
     return prediction_result1
+
+def add_rounded_edges(uploaded_file, radius=50):
+    """
+    Adds rounded corners to an uploaded image in Streamlit.
+
+    Parameters:
+    - uploaded_file: The file-like object uploaded via st.file_uploader.
+    - radius: The radius of the rounded corners.
+    """
+    # Open the uploaded image file
+    with Image.open(uploaded_file) as im:
+        # Create a rounded mask
+        mask = Image.new('L', im.size, 0)
+        draw = ImageDraw.Draw(mask)
+        # Draw four filled rectangles with the corner areas missing
+        draw.rectangle([(0, radius), (im.width, im.height - radius)], fill=255)
+        draw.rectangle([(radius, 0), (im.width - radius, im.height)], fill=255)
+        # Draw four filled circles in the corner areas
+        for x in (0, im.width - radius * 2):
+            for y in (0, im.height - radius * 2):
+                draw.pieslice([x, y, x + radius * 2, y + radius * 2], 0, 360, fill=255)
+        # Apply the rounded mask to the image
+        im = ImageOps.fit(im, mask.size, centering=(0.5, 0.5))
+        im.putalpha(mask)
+
+        # Save the output to a bytes object to return
+        im_bytes = BytesIO()
+        im.save(im_bytes, format='PNG')
+        im_bytes.seek(0)
+
+        return im_bytes
 
 with st.container():
     st.markdown("<center><h1>Eye Spy</h1></center>", unsafe_allow_html=True)
 
-    patient = st.text_input('Enter Patient Name', '', key="patient_name")
-    uploaded_file = st.file_uploader("Choose a CSV file")
+    col_first_name, col_last_name, = st.columns(2)
+
+    with col_first_name:
+        patient_first_name = st.text_input('First Name', '', key="patient_first_name")
+    with col_last_name:
+        patient_last_name = st.text_input('Last Name', '', key="patient_last_name")
+
+    uploaded_file = st.file_uploader("Select retina image file...")
     if uploaded_file is not None:
     # To read file as bytes:
         #bytes_data = uploaded_file.getvalue()
@@ -58,34 +95,40 @@ with st.container():
             #image = load_img(image_path, target_size=(224,224,3))
             image = load_img(f.name, target_size=(224,224,3))
             image = img_to_array(image)
+        rounded_image = add_rounded_edges(uploaded_file)
 
     submitted = st.button("Submit", key="submit_button")
 
     if submitted:
-        st.image(uploaded_file, caption=f"{patient}'s eye image")
+        st.image(rounded_image, caption=f"{patient_first_name} {patient_last_name}'s retinal image.")
         #code to show processing
         with st.spinner('Classifying...'):
             #perform prediction
             prediction_result = pred(image)
 
         #Display prediction result
-        st.write(f"Prediction: {prediction_result}")
+        # st.markdown(f"{prediction_result}")
 
         #stage 2 - return disease prediction if eye is unhealthy
-        if prediction_result == "Unhealthy":
+        if "Unhealthy" in prediction_result:
+            st.markdown("<span style='color: red; font-size: 24px;'>**Unhealthy**</span>", unsafe_allow_html=True)
             prediction_result_stage2 = pred2(image)
-            st.write(f"Anomalies: {prediction_result_stage2}")
-            disease_list = ['DR', 'MH', 'DN', 'ODC', 'Others']
+            # st.write(f"Anomalies: {prediction_result_stage2}")
+            disease_labels = ['DR','MH','DN','ODC','Other']
+            disease_list = ['Diabetic Retinopathy', 'Media Haze', 'Drusens', 'Optic Disc Cupping', 'Other']
             disease_dict = {
-                'DR':'Diabetic retinopathy is a microvascular complication of diabetes mellitus and is a leading cause of vision loss in the elderly and working population. The image is labeled as DR if it shows any of the following clinical findings: microaneurysms, retinal dot and blot hemorrhage, hard exudates or cotton wool spots.',
-                'MH':'Media Haze: The opacity of media can be a hallmark for the presence of cataracts, vitreous opacities, corneal edema or small pupils. Moreover, some other artifacts may be introduced as a result of acquisition procedures, such as eyelash artifacts and artifacts introduced by the instrument.',
-                'DN':'Drusens are yellow or white extracellular deposits located between the retinal pigment epithelium (RPE) and Bruch’s membrane. They naturally occur in the aged population. The presence of drusen is a hallmark and early sign of significant risk of age-related macular degeneration, geographic atrophy, choroidal neovascularization, and development of RPE abnormalities.',
-                'ODC':'Optic disc cupping is the thinning of neuroretinal rim such that optic disc appears excavated. Pathological ODC is generally referred to as glaucoma. However, several other non-glaucomatous diseases, such as arteritic anterior ischemic optic neuropathy and central retinal vein occlusion. Thus, it is very important to separately evaluate ODC.',
-                'Others':'The issue is none of the following: Diabetic retinopathy, Media Haze, Drusens, Optic disc cupping. Please visit an ophthalmologist.'
+                'Diabetic Retinopathy':'Diabetic retinopathy is a microvascular complication of diabetes mellitus and is a leading cause of vision loss in the elderly and working population. The image is labeled as DR if it shows any of the following clinical findings: microaneurysms, retinal dot and blot hemorrhage, hard exudates or cotton wool spots.',
+                'Media Haze':'Media Haze: The opacity of media can be a hallmark for the presence of cataracts, vitreous opacities, corneal edema or small pupils. Moreover, some other artifacts may be introduced as a result of acquisition procedures, such as eyelash artifacts and artifacts introduced by the instrument.',
+                'Drusens':'Drusens are yellow or white extracellular deposits located between the retinal pigment epithelium (RPE) and Bruch’s membrane. They naturally occur in the aged population. The presence of drusen is a hallmark and early sign of significant risk of age-related macular degeneration, geographic atrophy, choroidal neovascularization, and development of RPE abnormalities.',
+                'Optic Disc Cupping':'Optic disc cupping is the thinning of neuroretinal rim such that optic disc appears excavated. Pathological ODC is generally referred to as glaucoma. However, several other non-glaucomatous diseases, such as arteritic anterior ischemic optic neuropathy and central retinal vein occlusion. Thus, it is very important to separately evaluate ODC.',
+                'Other':'The issue is none of the following: Diabetic retinopathy, Media Haze, Drusens, Optic disc cupping. Please visit an ophthalmologist.'
                 }
             # st.write(prediction_result_stage2)
             selected_class = np.argmax(prediction_result_stage2)
             # st.write(selected_class)
-            st.write(f"Anomaly: **{disease_list[selected_class]}**")
-            st.write(f"\n")
+            st.write(f"Anomaly Type: **{disease_list[selected_class]}**")
+            st.write(f'Label: **{disease_labels[selected_class]}**')
+            # st.write(f"\n")
             st.write(f"{disease_dict[disease_list[selected_class]]}")
+        else:
+            st.markdown("<span style='color: green; font-size: 24px;'>**Healthy**</span>", unsafe_allow_html=True)
